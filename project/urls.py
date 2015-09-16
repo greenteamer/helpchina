02abc3ce6cf011f6838db2b9b54ba7495591303b
hechnya <hechnya@mail.ru>
2015-09-16 13:22:38 +0300

    вывели react пролукты и cartitems

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+
+
+from core.views import ProductViewSet, CartitemsViewSet
+
+router = routers.DefaultRouter()
+router.register(r'api/products', ProductViewSet)
+router.register(r'api/cartitems', CartitemsViewSet)
 
 urlpatterns = [
     # Examples:
@@ -8,5 +16,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include("core.urls")),
+
+    url(r'^', include(router.urls)),
+    url(r'^rest-api/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
